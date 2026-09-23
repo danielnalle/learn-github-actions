d35af5c316db8a8e3b5fc2f532c2b4f5d0bb26ed
danielnalle <danielnalle31@gmail.com>
2026-09-23 10:28:34 +0800

    fix: unit test of function add

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require("../src/calculator");
 
 test("adds two numbers", () => {
-  expect(add(2, 3)).toBe(100);
+  expect(add(2, 3)).toBe(5);
 });
 
 test("subtracts two numbers", () => {
