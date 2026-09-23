812532147c3c11580b8dd40d39359acd5b6511a9
danielnalle <danielnalle31@gmail.com>
2026-09-23 10:25:53 +0800

    test: simulate failing test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require("../src/calculator");
 
 test("adds two numbers", () => {
-  expect(add(2, 3)).toBe(5);
+  expect(add(2, 3)).toBe(100);
 });
 
 test("subtracts two numbers", () => {
